c74191176ef60447992f17dc498f5e7d9a48d646
git-savio1.domi <techsavvydominic3@gmail.com>
2023-08-09 10:21:55 +0300

    Add user authentication check in ApplicationController's before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::API
     include ActionController::Cookies
     protect_from_forgery with: :exception    
 
-
+    before_action :authenticate_user
 
 end
