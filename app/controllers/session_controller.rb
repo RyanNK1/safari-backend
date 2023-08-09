853d23555c34ae4dee6ef1261d038eac897076f6
git-savio1.domi <techsavvydominic3@gmail.com>
2023-08-09 11:11:41 +0300

    feat: Implement user session management actions (create and destroy) with appropriate handling

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,4 +2,28 @@ class SessionsController < ApplicationController
     skip_before_action :authenticate_user, only: [:create]
 
     wrap_parameters :user, include: %i[username password]
+
+    def create
+        @user = User.find_by(username: login_params[:username])
+      
+        if @user&.authenticate(login_params[:password])
+          session[:user_id] = @user.id
+  
+          render json: { user: UserSerializer.new(@user), status: 'ok' }, status: :accepted
+        else
+          render json: { message: 'Invalid username or password', status: 'error' }, status: :unauthorized
+        end
+    end
+    
+    def destroy
+        session.delete(:user_id)
+        
+        render json: { message: 'Logout successful' }, status: :ok
+      end
+    
+      private
+    
+      def login_params
+        params.require(:user).permit(:username, :password)
+    end
 end
