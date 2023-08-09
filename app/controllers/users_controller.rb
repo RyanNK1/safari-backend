88fd3e41b4148ee336d490053a85e8851a3b172c
git-savio1.domi <techsavvydominic3@gmail.com>
2023-08-09 11:34:26 +0300

    feat: Implement user-related actions (create, update, destroy, login, logout) and supporting methods in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,42 +6,65 @@ class UsersController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
     def index
-        user=User.all
-        render json: user
+        users=User.all
+        render json: users
     end
 
     def show
-        user = find_user
-        render json: user
+        render json: @user
     end
 
     def create
-        user = User.create(user_params)
-        render json: user, status: :created
+        @user = User.new(user_params)
+ 
+        if @user.save
+            token = encode_token({ user_id: @user.id })
+            render json: { user: UserSerializer.new(@user), token: token }, status: :created
+        else
+            render json: {errors: @user.errors.full_messages }, status: :unprocessable_entity
+     end
     end
     
     def update
-        user = find_user
-        user.update(user_params)
-        render json: user
-      end
+        if @user.update(user_params)
+            render json: UserSerializer.new(@user)
+        else
+            render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
     
-      
-    
-      def destroy
-        user= find_user
-        user.destroy
+    def destroy
+        @user.destroy
         head :no_content
+    end
+
+    def login
+        @user = User.find_by(username: login_params[:username])
+    
+        if @user&.authenticate(login_params[:password])
+          token = encode_token({ user_id: @user.id })
+          render json: { user: UserSerializer.new(@user) }, status: :accepted, token: token
+        else
+          render json: { message: 'Invalid username or password', status: 'error' }, status: :unauthorized
+        end
       end
 
-      private
+    def logout
+        render json: { message: 'Logout successful' }, status: :ok
+    end
 
-    def find_user
-        User.find(params[:id])
+    private
+
+    def set_user
+        @user.find(params[:id])
     end
     
     def user_params
         params.permit(:FirstName, :LastName, :Username, :DOB, :Email, :Password, :ConfirmPassword)
+    end
+
+    def login_params
+        params.permit(:username, :password)
     end
     
     def render_not_found_response
