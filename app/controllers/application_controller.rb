9f92c2694d560d630de8d326a043b009c584cbe8
git-savio1.domi <techsavvydominic3@gmail.com>
2023-08-10 21:00:22 +0300

    feat: Refactor ApplicationController for User and Organizer Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,40 +1,25 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
-    protect_from_forgery with: :exception    
-
-    before_action :authenticate_user
-
-    # Include the Stripe configuration
-    require 'stripe'
-    Stripe.api_key = 'sk_test_51NaYCPKKk5JD00V8GrPmR6uI8fhwZkdXvgGkF6kYaHcSOTA0g1dyoWhpFmkV2qBAH2CPDl8KiXYZfSqHRwiKEiQ3008ecRaBMs'
-
-
-    private
-
-    # This method is used to authenticate the user based on the user_id stored in the session or token.
-    def authenticate_user
-        unless !logged_in?
-            render json: { message: 'Unauthorized request', status: 'error' }, status: :unauthorized unless logged_in?
-        end
+    
+    # Retrieves the currently logged in admin, if any.
+    def admins
+        @admins ||= Organizer.find_by(id: cookies.signed[:organizer_id]) if cookies.signed[:organizer_id].present?
     end
 
-    # This method is used to get the currently logged-in user based on the user_id stored in the session or token.
+    # Retrieves the currently logged in user, if any.
     def current_user
-        @current_user ||= User.find_by(id: session[:user_id]) if logged_in?
+        @current_user ||= User.find_by(id: cookies.signed[:user_id]) if cookies.signed[:user_id].present?
     end
 
-    # This method checks if a user is logged in by using the current_user method.
+   # Checks if a user or an organizer is currently logged in.
     def logged_in?
-        !!session[:user_id]
+        current_user.present? || admins.present?
     end
-
-    # This method sets the current user in the session or token after successful authentication.
-    def log_in_user(user)
-        session[:user_id] = user.id
-    end
-
-    # This method logs out the current user by removing the user_id from the session or token.
-    def log_out_user
-        session.delete(:user_id)
+    
+    # Ensures that the user or organizer is logged in before accessing certain actions.
+    def require_login
+        unless logged_in?
+        render json: { error: "You must be logged in to access this page." }, status: :unauthorized
+        end
     end
 end
