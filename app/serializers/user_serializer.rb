a7dbe24745df1e79f19a1403ecd0c6e5d3011afd
git-savio1.domi <techsavvydominic3@gmail.com>
2023-08-10 23:26:46 +0300

    feat: Create UserSerializer for User Data Serialization

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-    attributes :id, :first_name, :last_name, :phone, :email, :username, :bio, :avatar, :created_at, :updated_at
+    attributes :id, :username
 end
