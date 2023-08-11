0867a55d011833d4835b4dbad36389da26f7495f
ryan <ryan.kimani0@gmail.com>
2023-08-11 16:25:48 +0300

    User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,51 +1,98 @@
+# class UsersController < ApplicationController
+#     before_action :require_login
+
+#     def index
+#         users = User.all
+#         render json: users
+#     end
+
+#     def show_me
+#         user = current_user
+
+#         if user
+#           render json: user
+#         else
+#           render json: { error: 'Not authorized' }, status: :unauthorized 
+#         end        
+#     end
+
+#     def create
+#         user = User.create(user_params)
+
+#         if user.valid?
+#           render json: user, status: :created
+#         else
+#           render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+#         end
+#     end
+    
+#     def homepage
+#         user = current_user
+#         if user
+#           render json: { message: 'Welcome to the user homepage!' }
+#         else
+#           render json: { error: 'Not authorized' }, status: :unauthorized
+#         end
+#     end
+    
+#     private
+    
+#     def user_params
+#         params.permit(:first_name, :last_name, :username, :dob, :email, :password, :confirm_password)
+#     end
+
+#     def login_params
+#         params.permit(:username, :password)
+#     end
+    
+#     def render_not_found_response
+#         render json: { error: "User not found" }, status: :not_found
+#     end
+# end
 class UsersController < ApplicationController
-    before_action :require_login
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-    def index
-        users = User.all
-        render json: users
+  def index
+      user=User.all
+      render json: user
+  end
+
+  def show
+      user=find_user
+      render json: user
+  end
+
+  def create
+      user=User.create(user_params)
+      render json: user, status: :created
+  end
+  
+  def update
+      user=find_user
+      user.update(user_params)
+      render json: user
     end
-
-    def show_me
-        user = current_user
-
-        if user
-          render json: user
-        else
-          render json: { error: 'Not authorized' }, status: :unauthorized 
-        end        
-    end
-
-    def create
-        user = User.create(user_params)
-
-        if user.valid?
-          render json: user, status: :created
-        else
-          render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-        end
-    end
+  
     
-    def homepage
-        user = current_user
-        if user
-          render json: { message: 'Welcome to the user homepage!' }
-        else
-          render json: { error: 'Not authorized' }, status: :unauthorized
-        end
+  
+    def destroy
+      user=find_user
+      user.destroy
+      head :no_content
     end
-    
+
     private
-    
-    def user_params
-        params.permit(:first_name, :last_name, :username, :dob, :email, :password, :confirm_password)
-    end
 
-    def login_params
-        params.permit(:username, :password)
-    end
-    
-    def render_not_found_response
-        render json: { error: "User not found" }, status: :not_found
-    end
+  def find_user
+      User.find(params[:id])
+  end
+  
+  def user_params
+      params.permit(:first_name, :last_name, :username, :dob, :email, :password, :confirm_password)
+  end
+  
+  def render_not_found_response
+      render json: { error: "User not found" }, status: :not_found
+  end
 end
+
